d13f7ce2b897010e0002017217730fbcd5edf4c8
David wanjohi <davidwagura83@gmail.com>
2024-05-28 10:50:05 +0300

    Add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
 
-  <div>
+  <div id="app">
     
     <router-view />
+
+    <nav-page/>
 
   </div>
   
 </template>
+
+<script>
+import NavPageVue from "./components/NavPage.vue";
+
+export default {
+
+  name: 'App',
+
+  components: {
+
+  NavPage: NavPageVue
+
+  }
+
+}
+</script>
---
+++ src/components/NavPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="bg-blue-400">hello</h1>
+    </div>
+</template>

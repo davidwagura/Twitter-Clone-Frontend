493886d9d1846a89c188d1727c63becec2132abb
David wanjohi <davidwagura83@gmail.com>
2024-05-30 20:24:53 +0300

    Create tweet container

--- src/components/Pages/ChildHome/ForYouPage.vue
+++ src/components/Pages/ChildHome/ForYouPage.vue
@@ -1,26 +1,57 @@
 <template>
 
-  <div class="p-4">
+  <div class="">
 
     <h2 class="text-2xl font-bold mb-4">For You</h2>
 
-    <!-- <li v-for="user in users" :key="user.id" class="mb-4 p-2 border rounded">
+    <div class="tweet-container">
 
-      <div class="font-bold">{{ user.first_name }} {{ user.last_name }}</div> -->
+      <ul v-if="tweets.length">
 
-      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="p-4 border hover:bg-gray-100">
 
-        <li v-for="tweet in tweets" :key="tweet.id" class="mb-2 p-2 border rounded">
+          <div class="flex items-center justify-between">
 
-          {{ tweet.body }}
+            <div class="flex items-center space-x-4">
+
+              <img :src="'./src'" alt="Avatar" class="w-12 h-12 rounded-full" />
+
+              <div v-if="tweet.user">
+
+                <div class="font-bold text-lg">{{ tweet.user.first_name }} {{ tweet.user.last_name }}</div>
+
+                <div class="text-gray-500">@{{ tweet.user.username }}</div>
+
+              </div>
+
+            </div>
+
+            <button @click="followUser(tweet.user.id)" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full">
+
+              Follow
+
+            </button>
+
+          </div>
+
+          <div class="p-4">
+
+            <div>{{ tweet.body }}</div>
+
+            <div class="text-gray-500 text-sm mt-2">{{ formatDate(tweet.created_at) }}</div>
+
+          </div>
 
         </li>
 
       </ul>
 
-    <!-- </li> -->
+      <div v-else class="text-gray-500">No tweets to display</div>
+
+    </div>
 
   </div>
+
 
 </template>
 
@@ -56,6 +87,14 @@ export default {
       console.log(response); 
 
       this.tweets = response.data.tweets;
+
+    },
+
+    formatDate(dateString) {
+
+      const options = { year: 'numeric', month: 'short', day: 'numeric' };
+
+      return new Date(dateString).toLocaleDateString(undefined, options);
 
     },
 

760c6b64673e4d7ed93a2c228ffe729d18778117
David wanjohi <davidwagura83@gmail.com>
2024-06-28 15:33:31 +0300

    Modify the border radius

--- src/components/Pages/trends/SearchBar.vue
+++ src/components/Pages/trends/SearchBar.vue
@@ -2,7 +2,7 @@
 
     <div>
 
-        <input class="w-full bg-gray-100 border pl-4 rounded-xl h-12" placeholder="Search" type="text">
+        <input class="w-full bg-gray-100 border pl-4 rounded-3xl h-12" placeholder="Search" type="text">
 
     </div>
 
